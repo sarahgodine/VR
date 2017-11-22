13dc9cf2a271acadcb7f4b5a490ff80a594ff5fb
MDXiao <michael.xiao@colorado.edu>
2017-11-21 23:40:35 -0700

    Created a new scene for rollercoaster idea

---
+++ Assets/Scripts/SplineWalker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplineWalker : MonoBehaviour {
+
+    public CurvedLineRenderer spline;
+
+    public float duration;
+
+    private float progress;
+   // private CurvedLinePoint[] linePoints = new CurvedLinePoint[0];
+
+    private void Update()
+    {
+        progress += Time.deltaTime / duration;
+        if (progress > 1f)
+        {
+            progress = 1f;
+        }
+        //transform.localPosition = spline.getPositions()[(int)progress];
+        Debug.Log("Progress: " + progress);
+        //Debug.Log("Line Points: " + spline.getPositions()[0]);
+    }
+}
